a4a667cb35c2c830d37b834c7d553d9e929585fc
Rohin Madhav <rohinmadhavk7@gmail.com>
2026-04-22 18:36:57 +0530

    server.js

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
+const transactionRouter = require("./routes/transactionRoutes");
 
 const app = express();
+connectDB();
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+app.use("/api/transaction", transactionRouter);
 
 const PORT = process.env.PORT || 5000;
 
