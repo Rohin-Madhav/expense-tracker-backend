ee98e1326bab9d4059bed19af0d2892d2f0c1c7a
Rohin Madhav <rohinmadhavk7@gmail.com>
2026-04-26 15:17:56 +0530

    authRouter added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const transactionRouter = require("./routes/transactionRoutes");
+const authRouter = require("./routes/userRoutes");
 
 const app = express();
 connectDB();
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api/transaction", transactionRouter);
+app.use("/api/user", authRouter);
 
 const PORT = process.env.PORT || 5000;
 
